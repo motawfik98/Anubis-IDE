59b2c5f85f7ab27faa29cfa16c793b88f2af2a6f
Mohamed Tawfik <motawfik98@outlook.com>
2020-10-09 19:28:19 +0200

    Saving credentials

---
+++ fastFunction.py
@@ -0,0 +1,14 @@
+def main():
+	first = False
+	second = 6.0
+	third = 2.0
+	name = """Mohamed Tawfik"""
+
+	if first:
+		print(second * third)
+	else:
+		print(second / third)
+	print(name)
+
+
+main()
